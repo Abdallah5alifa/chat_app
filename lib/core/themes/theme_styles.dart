6fe1ea0a698ed52d24aa117a24197565dbcfb5c5
Khalifa <Khalifa@DESKTOP-EIURBPD.com>
2024-10-02 20:29:31 +0300

    update splash screen

---
+++ lib/core/themes/theme_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TextStyleApp{
+
+
+TextStyle textStyle22 = const TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400
+);
+
+TextStyle textStyle30 = const TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.bold
+);
+
+
+
+}
--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:chat_app_task1/core/constants/assets_images.dart';
+import 'package:chat_app_task1/core/themes/theme_styles.dart';
 import 'package:chat_app_task1/features/auth/presentation/views/views_files/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -10,11 +12,11 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-      Timer(
-            const Duration(seconds: 3),
-                () =>
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (BuildContext context) =>const LoginScreen())));
+      // Timer(
+      //       const Duration(seconds: 3),
+      //           () =>
+      //       Navigator.of(context).push(MaterialPageRoute(
+      //           builder: (BuildContext context) =>const LoginScreen())));
 
 
     return Scaffold(
@@ -25,15 +27,30 @@ class SplashScreen extends StatelessWidget {
             children: [
               Center(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Image.asset(AssetsImages.logoImage,
-                    
                     height: 200,
                     width: 200,
                     ),
-                    SizedBox(height: 150,),
-                    const Text("The best chat app of this century")
+                    Text("WhatsUp",
+                    style: TextStyleApp().textStyle30.copyWith(
+                      color: Colors.green[600]
+                    ),
+                    ),
+                    const SizedBox(height: 170,),
+                     Text("The best chat app of this century",
+                    style: TextStyleApp().textStyle22,
+                    ),
+                    const SizedBox(height: 60,),
+                    const SizedBox(
+                      height: 60,
+                      child:  LoadingIndicator(
+                      indicatorType: Indicator.ballClipRotate, /// Required, The loading type of the widget
+                      colors:  [Colors.white],       /// Optional, The color collections
+                      strokeWidth: 5,                     /// Optional, The stroke of the line, only applicable to widget which contains line
+                      pathBackgroundColor: Colors.green   /// Optional, the stroke backgroundColor
+                                      ),
+                    )
                   ],
                 ),
               )
